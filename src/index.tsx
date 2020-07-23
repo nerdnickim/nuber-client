b2f0ec5d4f485abb4cbd55d6655d8a54886e5ae9
Nerdnickk <56470910+Nerdnickk@users.noreply.github.com>
2020-07-23 16:46:17 +0900

    connection app

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloProvider } from "react-apollo";
+import client from "./apollo";
+import App from "./Components/App";
+
+ReactDOM.render(
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>,
+	document.getElementById("root") as HTMLElement
+);
