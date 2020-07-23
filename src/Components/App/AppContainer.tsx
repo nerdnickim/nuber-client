6fb4a095c22793941654121cc58f9c065ad2be08
Nerdnickk <56470910+Nerdnickk@users.noreply.github.com>
2020-07-23 17:39:32 +0900

    globalstyle

--- src/Components/App/AppContainer.tsx
+++ src/Components/App/AppContainer.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import { graphql } from "react-apollo";
+import { createGlobalStyle } from "styled-components";
+import { reset } from "styled-reset";
 import AppPresenter from "./AppPresenter";
 import { IS_LOGGED_IN } from "./AppQueris";
+import { ThemeProvider } from "../../typed-components";
+import theme from "../../theme";
+
+// tslint:disable-next-line
+const GlobalStyle = createGlobalStyle`
+	${reset}
+`;
+
 const AppContainer = (data) => {
 	const { data: dataS } = data;
-	return <AppPresenter isLoggedIn={dataS.auth.isLoggedIn} />;
+	return (
+		<ThemeProvider theme={theme}>
+			<GlobalStyle />
+			<AppPresenter isLoggedIn={dataS.auth.isLoggedIn} />
+		</ThemeProvider>
+	);
 };
 
 export default graphql(IS_LOGGED_IN)(AppContainer);
